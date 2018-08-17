539105b3d82a6ab9fbefaf2be574c552e872b4c3
Philip Schmidt <philipschmidt.93@web.de>
2018-08-17 16:57:30 +0200

    Filled Graphics3 functions with Graphics4 function calls

--- Backends/Graphics3/G3onG4/Sources/Kore/G3onG4.cpp
+++ Backends/Graphics3/G3onG4/Sources/Kore/G3onG4.cpp
@@ -47,27 +47,33 @@ void Graphics3::setProjectionMatrix(const mat4& value) {
 // } /Fixed-function features
 
 void Graphics3::setVertexBuffer(VertexBuffer& vertexBuffer) {
-	// TODO
+	Graphics4::setVertexBuffer(vertexBuffer);
+	// TODO test	
 }
 
 void Graphics3::setVertexBuffers(VertexBuffer** vertexBuffers, int count) {
-	// TODO
+	Graphics4::setVertexBuffers(vertexBuffer);
+	// TODO test	
 }
 
 void Graphics3::setIndexBuffer(IndexBuffer& indexBuffer) {
-	// TODO
+	Graphics4::setIndexBuffer(indexBuffer);
+	// TODO test
 }
 
 void Graphics3::setTexture(TextureUnit unit, Texture* texture) {
-	// TODO
+	Graphics4::setTexture(unit, texture);
+	// TODO test
 }
 
 void Graphics3::drawIndexedVertices() {
-
+	Graphics4::drawIndexedVertices();
+	//TODO test
 }
 
 void Graphics3::drawIndexedVertices(int start, int count) {
-
+	Graphics4::drawIndexedVertices(start, count);
+	//TODO test
 }
 
 // OpenGL 1.X does not support hardware instancing
@@ -75,80 +81,94 @@ void Graphics3::drawIndexedVertices(int start, int count) {
 void drawIndexedVerticesInstanced(int instanceCount, int start, int count);*/
 
 void Graphics3::changeResolution(int width, int height) {
-
+	// TODO implement
+	// -> Graphics4::_resize(window, width, height)
 }
 
 bool Graphics3::hasWindow() {
-
+	// TODO implement
 }
 
 void Graphics3::setWindow(bool) {
-	
+	// TODO implement
 }
 	
 int Graphics3::antialiasingSamples() {
-	
+	Graphics4::antialiasingSamples();
+	//TODO test
 }
 	
 void Graphics3::setAntialiasingSamples(int samples) {
-	
+	Graphics4::antialiasingSamples(samples);
+	//TODO test
 }
 	
 
 bool Graphics3::renderTargetsInvertedY() {
-	
+	Graphics4::renderTargetsInvertedY();
+	//TODO test
 }
 	
 void Graphics3::setRenderTarget(RenderTarget* texture, int num = 0, int additionalTargets = 0) {
+	//Graphics4::setRenderTarget(RenderTarget* target);
+	//Graphics4::setRenderTargets(RenderTarget** targets, int count);
 	
+	//TODO implement
 }
 	
 void Graphics3::restoreRenderTarget() {
-	
+	Graphics4::restoreRenderTarget();
 }
 	
 
 // TODO (DK) windowId should be renamed contextId?
 void Graphics3::setup() {
-	
+	// TODO
 }
 
 void Graphics3::swapBuffers(int windowId = 0) {
-	
+	//TODO implement
+	//windowId ??
+	Graphics4::swapBuffers();
 }
 
 void Graphics3::begin(int windowId = 0) {
-	
+	Graphics4::begin(windowId);
+	//TODO test
 }
 
 void Graphics3::end(int windowId = 0) {
-	
+	Graphics4::end(windowId);
+	//TODO test
 }
 
 void Graphics3::makeCurrent(int windowId) {
-	
+	//TODO implement
 }
 
 void Graphics3::clearCurrent() {
-	
+	//TODO implement
 }
 
 
 void Graphics3::viewport(int x, int y, int width, int height) {
-	
+	Graphics4::viewport(x, y, width, height);
+	//TODO test
 }
 
 void Graphics3::scissor(int x, int y, int width, int height) {
-	
+	Graphics4::scissor(x, y, width, height);
+	//TODO test
 }
 
 void Graphics3::disableScissor() {
-	
+	Graphics4::disableScissor();
+	//TODO test
 }
 
 void Graphics3::setStencilParameters(ZCompareMode compareMode, StencilAction bothPass, StencilAction depthFail, StencilAction stencilFail, int referenceValue,
 						  int readMask = 0, int writeMask = 0) {
-							
+	// TODO implement						
 }
 
 
@@ -157,18 +177,22 @@ void Graphics3::setStencilParameters(ZCompareMode compareMode, StencilAction bot
 // Fixed-function material states (OpenGL 1.X) {
 
 void Graphics3::setMaterialState(MaterialState state, const vec4& value) {
+	// TODO implement						
 	
 }
 
 void Graphics3::setMaterialState(MaterialState state, float value) {
+	// TODO implement						
 	
 }
 
 void Graphics3::setTextureMapping(TextureUnit texunit, TextureMapping mapping, bool on) {
+	// TODO implement						
 	
 }
 
 void Graphics3::setTexCoordGeneration(TextureUnit texunit, TextureCoordinate texcoord, TexCoordGeneration generation) {
+	// TODO implement						
 	
 }
 
@@ -176,61 +200,72 @@ void Graphics3::setTexCoordGeneration(TextureUnit texunit, TextureCoordinate tex
 // } /Fixed-function material states
 
 void Graphics3::setRenderState(RenderState state, bool on) {
+	// TODO implement						
 	
 }
 
 void Graphics3::setRenderState(RenderState state, int v) {
+	// TODO implement						
 	
 }
 
 void Graphics3::setRenderState(RenderState state, float value) {
+	// TODO implement						
 	
 }
 
 void Graphics3::setTextureAddressing(TextureUnit unit, TexDir dir, TextureAddressing addressing) {
-	
+	Graphics4::setTextureAddressing(unit, dir, addressing);
+	//TODO test
 }
 
 void Graphics3::setTextureMagnificationFilter(TextureUnit texunit, TextureFilter filter) {
-	
+	Graphics4::setTextureMagnificationFilter(texunit, filter);
+	//TODO test
 }
 
 void Graphics3::setTextureMinificationFilter(TextureUnit texunit, TextureFilter filter) {
-	
+	Graphics4::setTextureMinificationFilter(texunit, filter);
+	//TODO test
 }
 
 void Graphics3::setTextureMipmapFilter(TextureUnit texunit, MipmapFilter filter) {
-	
+	Graphics4::setTextureMipmapFilter(texunit, filter);
+	//TODO test
 }
 
 void Graphics3::setBlendingMode(BlendingOperation source, BlendingOperation destination) {
+	// TODO implement						
 	
 }
 
 void Graphics3::setTextureOperation(TextureOperation operation, TextureArgument arg1, TextureArgument arg2) {
 	Graphics4::setTextureOperation(operation, arg1, arg2);
+	//TODO test
 }
 
 void Graphics3::setColorMask(bool red, bool green, bool blue, bool alpha) {
-	//TODO
+	// TODO implement						
 }
 
 
 bool Graphics3::vsynced() {
-	//TODO
+	// TODO implement						
 }
 
 unsigned Graphics3::refreshRate() {
-	//TODO
+	// TODO implement						
 }
 
 bool Graphics3::nonPow2TexturesSupported() {
 	Graphics4::nonPow2TexturesSupported();
+	// TODO test						
 }
 
 
 void Graphics3::clear(uint flags, uint color = 0, float depth = 1.0f, int stencil = 0) {
 	Graphics4::clear(flags, color, depth, stencil);	
+	// TODO test						
 }
 
 
@@ -242,9 +277,11 @@ void Graphics3::init(int windowId, int depthBufferBits, int stencilBufferBits, b
 
 void Graphics3::destroy(int windowId) {
 	Graphics4::destroy(windowId);
+	// TODO test						
 }
 
 
 void Graphics3::flush() {
 	Graphics4::flush();
+	// TODO test						
 }
